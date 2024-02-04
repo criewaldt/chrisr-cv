7721e02f53ed44fee4f9202d29b2c4e4511a60f8
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2024-02-04 03:35:27 -0500

    whitenoise

--- chrisr/settings.py
+++ chrisr/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'chrisr.urls'
@@ -119,6 +120,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
