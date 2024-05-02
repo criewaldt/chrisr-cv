6a812000c4bb8ae2a5a7721b0b969ea00878b5ea
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2024-05-02 01:44:46 -0400

    updates

--- resume/tasks.py
+++ resume/tasks.py
@@ -1,8 +1,5 @@
 from celery import shared_task
-from django.core.mail import send_mail, EmailMessage
-from django.conf import settings
-import os
-import time
+from django.core.mail import EmailMessage
 
 
 
@@ -17,15 +14,13 @@ def send_celery_email(email, num1, num2, delay_time, trickery):
         trickery_string = f"This was sent after {delay_time} seconds via Celery. "
         
     print(f'Celery sending email to {email}')
-    
-    time.sleep(delay_time)
-    
+        
     subject = 'Message from ChrisRiewaldt.com - Your Sum Calculation & My CV via Celery Task'
     message = f'ChrisRiewaldt.com celery task said: the sum of {num1} and {num2} equals {sum}. {trickery_string}Please see the download link to his CV here: \n https://chrisriewaldt.s3.amazonaws.com/website/CV/ChrisRResume.pdf \n\nThanks,\n-Chris Riewaldt'
-    email = EmailMessage(
+    email_msg = EmailMessage(
         subject,
         message,
         'criewaldt@gmail.com',
         [email]
     )
-    send_mail(subject, message, 'criewaldt@gmail.com', [email])
+    email_msg.send()
