c7f689eca06723235a685547a3a29fee4cb79ef1
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2024-05-02 01:48:25 -0400

    updates

--- resume/tasks.py
+++ resume/tasks.py
@@ -9,14 +9,14 @@ def send_celery_email(email, num1, num2, delay_time, trickery):
     sum = num1 + num2
     
     if trickery:
-        trickery_string = "Sorry, the delay time must be <= int(61). This was sent after the default delay of 60 seconds. "
+        trickery_string = "Sorry, the delay time must be <= int(61). This email was sent after the default delay of 60 seconds. "
     else:
-        trickery_string = f"This was sent after {delay_time} seconds via Celery. "
+        trickery_string = f"This email was sent after {delay_time} seconds via Celery. "
         
     print(f'Celery sending email to {email}')
         
     subject = 'Message from ChrisRiewaldt.com - Your Sum Calculation & My CV via Celery Task'
-    message = f'ChrisRiewaldt.com celery task said: the sum of {num1} and {num2} equals {sum}. {trickery_string}Please see the download link to his CV here: \n https://chrisriewaldt.s3.amazonaws.com/website/CV/ChrisRResume.pdf \n\nThanks,\n-Chris Riewaldt'
+    message = f'ChrisRiewaldt.com celery task said: the sum of {num1} and {num2} equals {sum}. {trickery_string}Please see the download link to his CV here: \n\nhttps://chrisriewaldt.s3.amazonaws.com/website/CV/ChrisRResume.pdf \n\nThanks,\n-Chris Riewaldt'
     email_msg = EmailMessage(
         subject,
         message,
