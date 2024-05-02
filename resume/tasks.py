8408c958f7ace5c1c76faffee476cb69868ad14d
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2024-05-02 01:34:03 -0400

    updates

--- resume/tasks.py
+++ resume/tasks.py
@@ -2,18 +2,28 @@ from celery import shared_task
 from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 import os
+import time
+
+
+
 @shared_task
-def send_celery_email(email, num1, num2):
-    cv_path = os.path.join(settings.BASE_DIR, 'static', 'ChrisR_Resume.pdf')
+def send_celery_email(email, num1, num2, delay_time, trickery):
         
     sum = num1 + num2
+    
+    if trickery:
+        trickery_string = "Sorry, the delay time must be <= int(61). This was sent after the default delay of 60 seconds. "
+    else:
+        trickery_string = f"This was sent after {delay_time} seconds via Celery. "
+    
+    time.sleep(delay_time)
+    
     subject = 'Message from ChrisRiewaldt.com - Your Sum Calculation & My CV via Celery Task'
-    message = f'ChrisRiewaldt.com celery task said: the sum of {num1} and {num2} equals {sum}. Also, please see his CV attached. \n\nThanks,\n-Chris Riewaldt'
+    message = f'ChrisRiewaldt.com celery task said: the sum of {num1} and {num2} equals {sum}. {trickery_string}Please see the download link to his CV here: \n https://chrisriewaldt.s3.amazonaws.com/website/CV/ChrisRResume.pdf \n\nThanks,\n-Chris Riewaldt'
     email = EmailMessage(
         subject,
         message,
         'criewaldt@gmail.com',
         [email]
     )
-    email.attach_file(cv_path)
     send_mail(subject, message, 'criewaldt@gmail.com', [email])
