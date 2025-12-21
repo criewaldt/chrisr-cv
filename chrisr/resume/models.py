15fcd1334adf3aba44d0621566aabcd422e378a4
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2025-12-21 18:20:22 -0500

    'first'

---
+++ chrisr/resume/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from bs4 import BeautifulSoup
+
+def strip_html(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    text = soup.get_text(separator=" ", strip=True)
+    return text
+
+class ProfessionalSummary(models.Model):
+    summary_html = models.TextField()
+    highlights = models.TextField()
+    
+    @property
+    def summary(self):
+        return strip_html(self.summary_html)
+
+    def __str__(self):
+        return self.summary
+    
+class Award(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class EmploymentHistory(models.Model):
+    job_title = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    description_html = models.TextField(blank=True)
+    is_current = models.BooleanField(default=False)
+    sort_order = models.IntegerField(blank=False, default=0)
+
+    @property
+    def description(self):
+        return strip_html(self.description_html)
+
+    def __str__(self):
+        return self.company_name
+
+class Education(models.Model):
+    degree = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.degree
+    
+class Keyword(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Resume(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    current_title = models.CharField(max_length=100, blank=True, null=True)
+    desired_title = models.CharField(max_length=100, blank=True, null=True)
+    professional_summary = models.ForeignKey(ProfessionalSummary, on_delete=models.CASCADE)
+    employment_history = models.ManyToManyField(EmploymentHistory)
+    awards = models.ManyToManyField(Award)
+    education = models.ManyToManyField(Education)
+    keywords = models.ManyToManyField(Keyword)
+
+    def __str__(self):
+        return self.name
+
+
---
+++ chrisr/resume/serializers.py
@@ -0,0 +1,84 @@
+from rest_framework import serializers
+from .models import Resume, ProfessionalSummary, EmploymentHistory, Award, Education, Keyword
+
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+        return representation
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = "__all__"
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+        return representation
+
+class KeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = "__all__"
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+        return representation
+
+class ProfessionalSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfessionalSummary
+        fields = ['summary', 'highlights']
+        
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+        return representation
+
+
+
+class EmploymentHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmploymentHistory
+        fields = ['job_title', 'company_name', 'location', 'start_date', 'end_date', 'description', 'is_current', 'sort_order'] 
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+        
+        return representation
+
+
+
+class ResumeSerializer(serializers.ModelSerializer):
+    professional_summary = ProfessionalSummarySerializer()
+    employment_history = EmploymentHistorySerializer(many=True, read_only=True)
+    education = EducationSerializer(many=True, read_only=True)
+    awards = AwardSerializer(many=True, read_only=True)
+    keywords = KeywordSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Resume
+        fields = ['name', 'email', 'phone', 'desired_title', 'current_title', 'professional_summary', 'employment_history', 'education', 'awards', 'keywords']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id', None)  # Remove 'id' from the response
+
+        # Sort employment history by 'sort_order' field in ascending order
+        if 'employment_history' in representation:
+            representation['employment_history'] = sorted(
+                representation['employment_history'],
+                key=lambda x: x.get('sort_order', 0)
+            )
+        
+        return representation
+
+
+    
