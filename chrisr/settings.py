055f8e2a2b723c2ec7e8cca90cb8257485dfdd6a
c.riewaldt@gmail.com <c.riewaldt@gmail.com>
2026-06-01 19:45:09 -0400

    updates

--- chrisr/settings.py
+++ chrisr/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -94,10 +95,12 @@ WSGI_APPLICATION = 'chrisr.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+        engine='django_cockroachdb',
+    )
 }
 
 
